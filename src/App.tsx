5ae78f9f43955e6468ede7f4c5d13c56256bd613
shodayuki <yshoda1227@gmail.com>
2020-07-29 08:20:35 +0900

    棒グラフ実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import LinePlot from "./components/LinePlot";
 import PiePlot from "./components/PiePlot";
+import BarPlot from './components/BarPlot';
 
 function App() {
   return (
     <div className="App">
       <LinePlot />
       <PiePlot />
+      <BarPlot />
     </div>
   );
 }
