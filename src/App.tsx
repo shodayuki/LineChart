1ce7f3d0ffac505e8d157c401517ed2c165cc19f
shodayuki <yshoda1227@gmail.com>
2020-07-29 23:51:52 +0900

    Radar Chart作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import LinePlot from "./components/LinePlot";
 import PiePlot from "./components/PiePlot";
 import BarPlot from './components/BarPlot';
+import RadarPlot from './components/RadarPlot';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <LinePlot />
       <PiePlot />
       <BarPlot />
+      <RadarPlot />
     </div>
   );
 }
